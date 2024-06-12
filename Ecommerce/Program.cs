d53516fb375b17e83ce84e0eb81d3465c403fdea
shravanimahendrakar <mahendrakarshravani@gmail.com>
2024-06-12 11:25:22 -0400

    Add project files.

---
+++ Ecommerce/Program.cs
@@ -0,0 +1,10 @@
+﻿using Ecommerce;
+
+Console.WriteLine("Hello, World!");
+
+
+Product p = new Product(112, "Mobile", 758.55, 2);
+p.IncreaseStock(3);
+Console.WriteLine($"Increase stock " + p.Stock);
+p.DecreaseStock(1);
+Console.WriteLine($"Decrease stock " + p.Stock);
